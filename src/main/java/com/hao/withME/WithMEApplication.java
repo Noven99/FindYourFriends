3269bd9e70af0f2b03d66bef7b37017b7645e23c
Noven <t19991103@163.com>
2025-12-13 13:05:19 +0000

    根据标签查询用户

---
+++ src/main/java/com/hao/withME/WithMEApplication.java
@@ -0,0 +1,15 @@
+package com.hao.withME;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.hao.usercenter.mapper")
+public class WithMEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithMEApplication.class, args);
+    }
+
+}
